c9d22fe58b90cc914a2dc46cfcf17ae0351c72e2
mouhcine_soufi <mohciness3@gmail.com>
2020-12-28 22:09:25 +0100

    TP2-backend as a service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,37 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { MangasComponent } from './components/mangas/mangas.component';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFireMessagingModule } from '@angular/fire/messaging';
+import { AngularFireStorageModule} from '@angular/fire/storage';
+import {environment} from '../environments/environment';
+import { FormsModule } from '@angular/forms';
+import { AuthService } from './service/auth.service';
+
+
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    MangasComponent
+    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
+    AngularFireMessagingModule,
+    AngularFireStorageModule,
+    FormsModule
+
   ],
-  providers: [],
+  providers: [
+    AngularFirestore,
+    AuthService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase : {
+    apiKey: "AIzaSyAIHAHH3fn_WlTpAnTC05L2yWbwEYoydoY",
+    authDomain: "mouhcine-tp.firebaseapp.com",
+    projectId: "mouhcine-tp",
+    storageBucket: "mouhcine-tp.appspot.com",
+    messagingSenderId: "948687765840",
+    appId: "1:948687765840:web:63605602eb4ff547122c55",
+    measurementId: "G-3834YKTSGN"
+  }
 };
 
 /*
